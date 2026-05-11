97cf4f6f9d6680abb23ff1a50ffc1c5fe79873c2
immigreproduction-del <immigreproduction@gmail.com>
2026-05-11 02:31:09 +0200

    Update config.py

--- config.py
+++ config.py
@@ -12,14 +12,51 @@ DM_SPAM_MESSAGE = "Doucement le spam 😭"
 
 
 # =========================
-# BOT STATUS
+# STATUS
 # =========================
 
-BOT_STATUS = "Surveille les casse-couilles 👁️"
+STATUS_CHANGE_MINUTES = 10
 
 
-# =========================
-# LOGS
-# =========================
-
-ENABLE_LOGS = True
+STATUSES = [
+    "Enfant de Misty 👁️",
+    "Je vois tout",
+    "Le serveur est observé",
+    "Quelqu’un prépare encore une bêtise",
+    "Je garde un œil sur le chat 👀",
+    "Le calme est fragile",
+    "Je surveille les comportements",
+    "Le serveur dort sous ma surveillance",
+    "Rien ne m’échappe",
+    "Observation silencieuse",
+    "Je protège cet endroit",
+    "Je lis entre les lignes",
+    "Le prochain faux mouvement approche",
+    "Quelqu’un écrit beaucoup trop 😭",
+    "Je regarde sans parler",
+    "Misty m’a donné une mission",
+    "Le serveur est sous contrôle",
+    "Le chaos est contenu",
+    "Je détecte les anomalies",
+    "Je sais déjà ce qu’il se passe",
+    "Le silence est suspect",
+    "Toujours en train d’observer 👁️",
+    "Je vois même les messages supprimés",
+    "Quelqu’un va encore se faire timeout",
+    "Je maintiens l’équilibre",
+    "Je regarde qui dépasse les limites",
+    "Le prochain incident est proche",
+    "Surveillance active",
+    "Le serveur respire enfin",
+    "Je garde les portes",
+    "Je vois les tensions monter",
+    "Mistybot veille 🌙",
+    "Je garde le silence… pour l’instant",
+    "Je n’oublie rien",
+    "Le désordre attire mon attention",
+    "Je protège le calme",
+    "Je suis déjà au courant",
+    "Le serveur est sous ma garde",
+    "La modération ne dort jamais",
+    "Je regarde qui abuse",
+]
