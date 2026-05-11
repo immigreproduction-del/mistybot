facaebf7484c7a639d48638e16830c7659f429d4
immigreproduction-del <immigreproduction@gmail.com>
2026-05-11 02:17:22 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+# =========================
+# ANTI SPAM
+# =========================
+
+MESSAGE_LIMIT = 5
+
+TIME_WINDOW_SECONDS = 60
+
+TIMEOUT_MINUTES = 2
+
+DM_SPAM_MESSAGE = "Doucement le spam 😭"
+
+
+# =========================
+# BOT STATUS
+# =========================
+
+BOT_STATUS = "Surveille les casse-couilles 👁️"
+
+
+# =========================
+# LOGS
+# =========================
+
+ENABLE_LOGS = True
